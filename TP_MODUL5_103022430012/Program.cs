103df50fdb0277a486f3e96755eeaa06eba2a912
azaa-x <n.andrean119@gmail.com>
2026-03-29 00:28:10 +0700

    merge all

--- TP_MODUL5_103022430012/Program.cs
+++ TP_MODUL5_103022430012/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Class pertama
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+// Class kedua
+class DataGeneric<T>
+{
+    public T Data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Masukkan nama: ");
+        string X = Console.ReadLine();
+
+        Console.Write("Masukkan NIM: ");
+        string Y = Console.ReadLine();
+
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(X);
+
+        DataGeneric<string> data = new DataGeneric<string>(Y);
+        data.PrintData();
+
+        Console.ReadLine();
+    }
+}
